30dc80be7d1aec29247d05eacd91b9effbc5fa41
totchina <383144602@qq.com>
2015-04-07 18:53:44 +0800

    天气utility和布局文件

--- src/com/binge/coolweather/util/Utility.java
+++ src/com/binge/coolweather/util/Utility.java
@@ -1,5 +1,15 @@
 package com.binge.coolweather.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 
 import com.binge.coolweather.db.CoolWeatherDB;
@@ -8,11 +18,12 @@ import com.binge.coolweather.model.County;
 import com.binge.coolweather.model.Province;
 
 public class Utility {
-	public synchronized static boolean handleProvincesResponse(CoolWeatherDB coolWeatherDB,String response){
-		if(!TextUtils.isEmpty(response)){
+	public synchronized static boolean handleProvincesResponse(
+			CoolWeatherDB coolWeatherDB, String response) {
+		if (!TextUtils.isEmpty(response)) {
 			String[] allProvinces = response.split(",");
-			if(allProvinces != null && allProvinces.length>0){
-				for(String p :allProvinces){
+			if (allProvinces != null && allProvinces.length > 0) {
+				for (String p : allProvinces) {
 					String[] array = p.split("\\|");
 					Province province = new Province();
 					province.setProvinceCode(array[0]);
@@ -24,12 +35,13 @@ public class Utility {
 		}
 		return false;
 	}
-	
-	public static boolean handleCitiesResponse(CoolWeatherDB coolWeatherDB,String response,int provinceId){
-		if(!TextUtils.isEmpty(response)){
+
+	public static boolean handleCitiesResponse(CoolWeatherDB coolWeatherDB,
+			String response, int provinceId) {
+		if (!TextUtils.isEmpty(response)) {
 			String[] allCities = response.split(",");
-			if(allCities !=null && allCities.length>0){
-				for(String c : allCities){
+			if (allCities != null && allCities.length > 0) {
+				for (String c : allCities) {
 					String[] array = c.split("\\|");
 					City city = new City();
 					city.setCityCode(array[0]);
@@ -42,12 +54,13 @@ public class Utility {
 		}
 		return false;
 	}
-	
-	public static boolean handleCountiesResponse(CoolWeatherDB coolWeatherDB,String response,int cityId){
-		if(!TextUtils.isEmpty(response)){
-			String[] allCounties  =  response.split(",");
-			if(allCounties != null && allCounties.length>0){
-				for(String c: allCounties){
+
+	public static boolean handleCountiesResponse(CoolWeatherDB coolWeatherDB,
+			String response, int cityId) {
+		if (!TextUtils.isEmpty(response)) {
+			String[] allCounties = response.split(",");
+			if (allCounties != null && allCounties.length > 0) {
+				for (String c : allCounties) {
 					String[] array = c.split("\\|");
 					County county = new County();
 					county.setCountyCode(array[0]);
@@ -59,5 +72,39 @@ public class Utility {
 			}
 		}
 		return false;
+	}
+
+	public static void handleWeatherResponse(Context context, String response) {
+		try {
+			JSONObject jsonObject = new JSONObject(response);
+			JSONObject weatherInfo = jsonObject.getJSONObject("weatherinfo");
+			String cityName = weatherInfo.getString("city");
+			String weatherCode = weatherInfo.getString("cityid");
+			String temp1 = weatherInfo.getString("temp1");
+			String temp2 = weatherInfo.getString("temp2");
+			String weatherDesp = weatherInfo.getString("weather");
+			String publishTime = weatherInfo.getString("ptime");
+			saveWeatherInfo(context, cityName, weatherCode, temp1, temp2,
+					weatherDesp, publishTime);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void saveWeatherInfo(Context context, String cityName,
+			String weatherCode, String temp1, String temp2, String weatherDesp,
+			String publishTime) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyƒÍM‘¬d»’", Locale.CHINA);
+		SharedPreferences.Editor editor = PreferenceManager
+				.getDefaultSharedPreferences(context).edit();
+		editor.putBoolean("city_selected", true);
+		editor.putString("city_name", cityName);
+		editor.putString("weather_code", weatherCode);
+		editor.putString("temp1", temp1);
+		editor.putString("temp2", temp2);
+		editor.putString("weather_desp", weatherDesp);
+		editor.putString("publish_time", publishTime);
+		editor.putString("current_date", sdf.format(new Date()));
+		editor.commit();
 	}
 }
